9cdfc1c8f7b01f2c943faa4c91dd064ab9ed302f
tetrider <tetrider91@gmail.com>
2019-01-07 01:50:01 +0800

    day 10.1

--- engine.py
+++ engine.py
@@ -125,6 +125,7 @@ def play_game(player, entities, game_map, message_log, game_state, con, panel,
             elif game_state == GameStates.TARGETING:
                 player_turn_results.append({'targeting_cancelled': True})
             else:
+                save_game(player, entities, game_map, message_log, game_state)
                 return True
 
         if fullscreen:
