d2953796519afb380a4790a020482208f0a2ecee
tetrider <tetrider91@gmail.com>
2019-01-09 06:53:40 +0800

    day 12

---
+++ random_utils.py
@@ -0,0 +1,29 @@
+from random import randint
+
+
+def from_dungeon_level(table, dungeon_level):
+    for (value, level) in reversed(table):
+        if dungeon_level >= level:
+            return value
+
+    return 0
+
+
+def random_choice_index(chances):
+    random_chance = randint(1, sum(chances))
+
+    running_sum = 0
+    choice = 0
+    for w in chances:
+        running_sum += w
+
+        if random_chance <= running_sum:
+            return choice
+        choice += 1
+
+
+def random_choice_from_dict(choise_dict):
+    choices = list(choise_dict.keys())
+    chances = list(choise_dict.values())
+
+    return choices[random_choice_index(chances)]
